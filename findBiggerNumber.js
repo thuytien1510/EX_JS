ea39d42417f7cc8948bb873b8855332485bbe0ce
thuytien1510 <thuytiencoder@gmail.com>
2022-06-19 21:50:59 +0700

    js

--- findBiggerNumber.js
+++
@@ -1,7 +0,0 @@
-//cho a, b la 2 so duoc so sanh
-//ham so sanh
-const biggerNumber = (a, b) => {
-    a > b ? console.log(`${a} la so lon nhat`) : 
-    a < b ?  console.log(`${b} la so lon nhat`) : console.log('hai so bang nhau')
-}
-biggerNumber(9.5, 7.9)
